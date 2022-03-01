62f5f0812554435aea13cb2555d45acc7e35d100
Allen Su <allenysu@gmail.com>
2022-03-01 16:43:54 -0500

    Initial Commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Allen Su on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
